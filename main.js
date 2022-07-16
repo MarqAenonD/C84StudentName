cb3d1197e530995a61f02a285e888f9bcaf9dd48
MarqAenonD <106565814+MarqAenonD@users.noreply.github.com>
2022-07-15 19:19:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function submit()
 {
     var name_1 = document.getElementById("name_of_the_student1").value;
     var name_2 = document.getElementById("name_of_the_student2").value;
-    var name_3 = document.getElementById("name_of_the_student3").value;
+    document.getElementById("scores").value = val; document.getElementById("student_name").value = "";
     var name_4 = document.getElementById("name_of_the_student4").value;
 
     name_of_the_student_array.push(name_1);
